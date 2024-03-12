a583bd2e3e3f307fe63706869ae3f36d7cfafd51
alexclapou <alex.clapou@yahoo.com>
2024-03-13 01:08:33 +0200

    google oauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,8 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-developer-strategy
+  skip_before_action :verify_authenticity_token, only: :google_oauth2
+
+  def google_oauth2
+    binding.pry
+  end
+end
