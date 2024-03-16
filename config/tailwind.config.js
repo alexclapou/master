72356f2171673cf2cdaacd91d2668a3048e6b600
alexclapou <alex.clapou@yahoo.com>
2024-03-16 15:00:51 +0200

    more nav + fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,41 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Helvetica Neue",
+          "Helvetica",
+          "Roboto",
+          "Arial",
+          "sans-serif",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      height: {
+        19: "73px",
+      },
+      maxWidth: {
+        pagemax: "1246px",
+      },
+      padding: {
+        45: "18px",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
 }
