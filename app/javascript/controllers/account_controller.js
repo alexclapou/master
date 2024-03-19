cd81c4499a0f29a83a9910d58515bad53b040369
alexclapou <alex.clapou@yahoo.com>
2024-03-20 01:59:36 +0200

    highlight

--- app/javascript/controllers/account_controller.js
+++ app/javascript/controllers/account_controller.js
@@ -1,21 +1,32 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import hljs from "highlight.js";
 
 export default class extends Controller {
-  static targets = ["button", "dropdown"]
+  static targets = ["button", "dropdown"];
+
+  connect() {
+    hljs.configure({
+      ignoreUnescapedHTML: true,
+    });
+
+    document.querySelectorAll("pre").forEach((el) => {
+      hljs.highlightElement(el);
+    });
+  }
 
   toggle_dropdown(event) {
-    const button_clicked = this.buttonTarget.contains(event.target)
+    const button_clicked = this.buttonTarget.contains(event.target);
 
-    if (button_clicked) this.dropdownTarget.classList.toggle("hidden")
+    if (button_clicked) this.dropdownTarget.classList.toggle("hidden");
   }
 
   close_dropdown(event) {
-    const button_clicked = this.buttonTarget.contains(event.target)
-    const dropdown_clicked = this.dropdownTarget.contains(event.target)
+    const button_clicked = this.buttonTarget.contains(event.target);
+    const dropdown_clicked = this.dropdownTarget.contains(event.target);
 
-    if (dropdown_clicked || (button_clicked && event.type == "click")) return
+    if (dropdown_clicked || (button_clicked && event.type == "click")) return;
 
-    this.dropdownTarget.classList.add("hidden")
-    this.buttonTarget.blur()
+    this.dropdownTarget.classList.add("hidden");
+    this.buttonTarget.blur();
   }
 }
