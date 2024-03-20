4163d4eac59dbde37150ba6f77c06991b69f33d2
alexclapou <alex.clapou@yahoo.com>
2024-03-21 00:51:09 +0200

    more toolbar changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,4 +16,25 @@ addEventListener("trix-initialize", function (event) {
     "beforeend",
     '<button type="button" class="trix-button trix-button--icon trix-button--icon-highlight" data-trix-attribute="highlight" data-trix-key="y" title="Highlight" tabindex="-1">${lang.highlight}'
   );
+
+  const TOOLBAR_BUTTON_ICONS = [{ identifier: ".trix-button--icon-bold", icon: "B" }];
+  TOOLBAR_BUTTON_ICONS.forEach((group) => {
+    event.target.toolbarElement.querySelector(group.identifier).textContent = "asd";
+    console.log(event.target.toolbarElement.querySelector(group.identifier).textContent);
+  });
+
+  window.onscroll = function () {
+    stickyToolbar();
+  };
+
+  var toolbar = document.querySelector("trix-toolbar");
+  var sticky = toolbar.offsetTop;
+
+  function stickyToolbar() {
+    if (window.scrollY + 50 >= sticky) {
+      toolbar.classList.add("stickytab");
+    } else {
+      toolbar.classList.remove("stickytab");
+    }
+  }
 });
