e67e925c0e17ec6781e6841813f7af5d352faf70
alexclapou <alex.clapou@yahoo.com>
2024-03-21 23:52:48 +0200

    nice bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,17 +11,10 @@ addEventListener("trix-initialize", function (event) {
   var groupElement = event.target.toolbarElement.querySelector(
     ".trix-button-group.trix-button-group--text-tools"
   );
-
   groupElement.insertAdjacentHTML(
     "beforeend",
     '<button type="button" class="trix-button trix-button--icon trix-button--icon-highlight" data-trix-attribute="highlight" data-trix-key="y" title="Highlight" tabindex="-1">${lang.highlight}'
   );
-
-  const TOOLBAR_BUTTON_ICONS = [{ identifier: ".trix-button--icon-bold", icon: "B" }];
-  TOOLBAR_BUTTON_ICONS.forEach((group) => {
-    event.target.toolbarElement.querySelector(group.identifier).textContent = "asd";
-    console.log(event.target.toolbarElement.querySelector(group.identifier).textContent);
-  });
 
   window.onscroll = function () {
     stickyToolbar();
@@ -33,8 +26,12 @@ addEventListener("trix-initialize", function (event) {
   function stickyToolbar() {
     if (window.scrollY + 50 >= sticky) {
       toolbar.classList.add("stickytab");
+      toolbar.classList.remove("notstickybar");
     } else {
-      toolbar.classList.remove("stickytab");
+      if (toolbar.classList.contains("stickytab")) {
+        toolbar.classList.remove("stickytab");
+        toolbar.classList.add("notstickybar");
+      }
     }
   }
 });
