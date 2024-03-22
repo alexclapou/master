7077e2e2ec1c839afd5e70e4375daad469cd1f9d
alexclapou <alex.clapou@yahoo.com>
2024-03-22 09:56:42 +0200

    start tags

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,24 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["title", "title_length"];
+  static targets = ["title", "tags", "title_length"];
+
+  add_tags(event) {
+    console.log(event.key);
+  }
 
   check_limit() {
     // set rows to 1 => we calculate them after
     this.titleTarget.rows = 1;
 
     // resize it as much as we need
-    while (this.should_resize(this.titleTarget)) {
-      this.resize(this.titleTarget);
-    }
+    this.resize_max(this.titleTarget);
 
     // there is a bug, where a row is added even if we are not at the end of line (1/2 charcters left to fill the row)
     // so in this case, just remove a row (the possible new row added - bug) and resize if needed after
     this.titleTarget.rows = Math.max(1, this.titleTarget.rows - 1);
 
-    if (this.should_resize(this.titleTarget)) {
-      this.resize(this.titleTarget);
-    }
+    this.resize_if_need(this.titleTarget);
 
     this.title_lengthTarget.innerHTML = this.titleTarget.value.length;
   }
@@ -30,6 +30,18 @@ export default class extends Controller {
   resize(textarea) {
     if (this.should_resize(textarea)) {
       textarea.rows++;
+    }
+  }
+
+  resize_max(textarea) {
+    while (this.should_resize(textarea)) {
+      this.resize(textarea);
+    }
+  }
+
+  resize_if_need(textarea) {
+    if (this.should_resize(textarea)) {
+      this.resize(textarea);
     }
   }
 }
