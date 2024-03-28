8c35f97f648ad0e642f14c783540efd73d4d31f4
alexclapou <alex.clapou@yahoo.com>
2024-03-29 01:26:19 +0200

    sort

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,6 +2,7 @@ class Story < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
   has_rich_text :content
+  default_scope { order(created_at: :asc) }
 
   def display_date
     created_at.strftime("%B #{created_at.day.ordinalize}, %Y")
