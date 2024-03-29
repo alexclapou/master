ce12cd0aa9300209dcdb064343e6fa75febb4417
alexclapou <alex.clapou@yahoo.com>
2024-03-29 11:44:18 +0200

    add toggle

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -123,7 +123,7 @@ export default class extends Controller {
         formatted_input = formatted_input.replace(/\.*$/g, "");
 
         chip.textContent = formatted_input;
-        chip.style.border = "1px solid rgb(229, 229, 229)";
+        chip.style.border = "1px solid var(--color-text-fade)";
         chip.setAttribute("data-value", "makai");
 
         // Adding classes for chip styling
