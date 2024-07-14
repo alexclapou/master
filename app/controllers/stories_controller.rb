64c45d05811214fb92106b089720dbadaaa44e14
alexclapou <alex.clapou@yahoo.com>
2024-07-14 14:07:11 +0300

    authentication before authorization

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
+  before_action :authenticate_user!, except: %w[show] # debug using honeybadger
   load_and_authorize_resource
-  before_action :authenticate_user!, except: %w[show]
   before_action :load_story, only: %w[show edit update preview publish]
 
   def new
