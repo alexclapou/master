ac221b2a8bfc408c09bcd5a82e53739ae7c505ec
alexclapou <alex.clapou@yahoo.com>
2024-07-24 16:23:29 +0300

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def index
+    @search_term = params[:q]
+    return unless @search_term
+
+    @stories = Story.joins(:rich_text_content)
+                    .where("action_text_rich_texts.body ILIKE ?", "%#{@search_term}%")
+                    .or(Story.where("title ilike ? ", "%#{@search_term}%"))
+    @users = User.where("email ilike ?", "%#{@search_term}%")
+    @tags = Tag.where("name ilike ?", "%#{@search_term}%")
   end
 end
