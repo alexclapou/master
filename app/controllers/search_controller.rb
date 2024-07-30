acb491cad18d9c9115b8e2477c92159b535bff1c
alexclapou <alex.clapou@yahoo.com>
2024-07-30 23:24:55 +0300

    asd

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,18 +8,19 @@ class SearchController < ApplicationController
     @stories = Story.all.joins(:rich_text_content)
                     .where("action_text_rich_texts.body ILIKE ?", "%#{@search_term}%")
                     .or(Story.published.where("title ilike ? ", "%#{@search_term}%"))
-    render @stories
+    render partial: "stories/list_view"
   end
 
   def search_users
     @search_term = params[:q]
-    @users = User.where("email ilike ?", "%#{@search_term}%")
-    render @users
+    # @users = User.where("email ilike ?", "%#{@search_term}%")
+    @users = User.where("substring(email from 1 for position('@' in email) - 1) ilike ?", "%#{@search_term}%")
+    render partial: "users/list_view"
   end
 
   def search_tags
     @search_term = params[:q]
     @tags = Tag.where("name ilike ?", "%#{@search_term}%")
-    render @tags
+    render partial: "tags/list_view"
   end
 end
