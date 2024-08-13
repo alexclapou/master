928a96b4d6ed630269a090d33e548e720d2415cd
alexclapou <alex.clapou@yahoo.com>
2024-08-13 22:36:24 +0300

    pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,21 +6,24 @@ class SearchController < ApplicationController
   def search_stories
     @search_term = params[:q]
     @stories = Story.published.joins(:rich_text_content)
-                    .where("action_text_rich_texts.body ILIKE ?", "%#{@search_term}%")
-                    .or(Story.published.where("title ilike ? ", "%#{@search_term}%"))
+                    .where("action_text_rich_texts.body ILIKE ?", "%#{@search_term}%").paginate(page: params[:stories_page], per_page: 10)
+    # .or(Story.published.where("title ilike ? ",
+    #                           "%#{@search_term}%")).paginate(page: params[:stories_page], per_page: 9)
     render partial: "stories/list_view"
   end
 
   def search_users
     @search_term = params[:q]
     # @users = User.where("email ilike ?", "%#{@search_term}%")
-    @users = User.where("substring(email from 1 for position('@' in email) - 1) ilike ?", "%#{@search_term}%")
+    @users = User.where("substring(email from 1 for position('@' in email) - 1) ilike ?", "%#{@search_term}%").paginate(
+      page: params[:page], per_page: 10
+    )
     render partial: "users/list_view"
   end
 
   def search_tags
     @search_term = params[:q]
-    @tags = Tag.where("name ilike ?", "%#{@search_term}%")
+    @tags = Tag.where("name ilike ?", "%#{@search_term}%").paginate(page: params[:page], per_page: 9)
     render partial: "tags/list_view"
   end
 end
