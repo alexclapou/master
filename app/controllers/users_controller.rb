0121c6ff6e85a72da5357c3cd61bd2ec7412a21b
alexclapou <alex.clapou@yahoo.com>
2024-08-13 19:37:41 +0300

    q'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,5 @@ class UsersController < ApplicationController
     @stories = @user.stories.paginate(page: params[:stories_page], per_page: 5)
     @likes = @user.likes.paginate(page: params[:likes_page], per_page: 5)
     @comments = @user.comments.paginate(page: params[:comments_page], per_page: 5)
-    @search_term = "1"
   end
 end
