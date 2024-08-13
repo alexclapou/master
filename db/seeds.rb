fd8041757c3011f2a80500d3efa0175bbedcaf2c
alexclapou <alex.clapou@yahoo.com>
2024-08-13 21:35:13 +0300

    seed+zindex

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@ Tag.create(tags.map { |tag| { name: tag } })
 
 puts "creating users...".colorize(:light_green)
 users = []
+users << { email: "alex@yahoo.com", password: "parola", first_name: "Alex", last_name: "Clapou" }
 50.times do |idx|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   email = Faker::Internet.email(name: "#{first_name} #{last_name}")
-  users << { email: email, password: "parola", first_name: first_name, last_name: last_name }
+  users << { email:, password: "parola", first_name:, last_name: }
 end
 User.create(users)
 
 puts "creating stories...".colorize(:light_green)
-
 
 puts "done".colorize(:light_green)
