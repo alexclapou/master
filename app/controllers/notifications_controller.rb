20f313b89cd26c5f03836d02ae70fe579459710e
alexclapou <alex.clapou@yahoo.com>
2024-08-15 18:47:28 +0300

    asd

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,8 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.order(created_at: :desc).includes(item: %i[story comment]).paginate(
+      page: params[:page], per_page: 10
+    )
   end
 end
