e1d0796215df8c175590712c3361fa4271ba2745
alexclapou <alex.clapou@yahoo.com>
2024-08-16 18:49:57 +0300

    consumer

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,4 +1,4 @@
-import consumer from "./consumer";
+import consumer from "channels/consumer";
 
 const notificationChannel = consumer.subscriptions.create("NotificationChannel", {
   received(data) {
